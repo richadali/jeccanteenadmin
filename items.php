dc825767431db8460e6c8ff33f148f2c30c55a33
JASPREET SINGH <38997592+Jaspreet8843@users.noreply.github.com>
2022-02-21 22:24:56 +0530

    Update items.php

--- items.php
+++ items.php
@@ -3,24 +3,24 @@
 
 <!-- !PAGE CONTENT! -->
 <div class="w3-main" style="margin-left:300px;margin-top:43px;">
-<header class="w3-container" style="padding-top:22px">
-    <h4><b><i class="fa fa-list " aria-hidden="true"></i>  All Menu Items</b></h4>
+  <header class="w3-container" style="padding-top:22px">
+    <h4><b><i class="fa fa-list " aria-hidden="true"></i> All Menu Items</b></h4>
   </header>
-<center>
-<div style="padding-top: 50px; font-family: 'Roboto', sans-serif;"></div>
-<table class="w3-table-all" style="width: 60%;font-family: 'Roboto', sans-serif;"" >
+  <center>
+    <div style="padding-top: 50px; font-family: 'Roboto', sans-serif;"></div>
+    <table class="w3-table-all" style="width: 60%;font-family: 'Roboto', sans-serif;"" >
 <thead>
-  <tr class="w3-green">
-    <th>Slno</th>
-    <th></th>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Action</th>
-  </tr>
-  </thead>
-  <tbody id="myTable">
-  </tbody>
-</table>
+  <tr class=" w3-green">
+      <th>Slno</th>
+      <th></th>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Action</th>
+      </tr>
+      </thead>
+      <tbody id="myTable">
+      </tbody>
+    </table>
 </div>
 
 
@@ -60,60 +60,81 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script type="module">
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getFirestore, query, where, doc, getDoc, getDocs,setDoc, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-storage.js";
+  import {
+    initializeApp
+  } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+  import {
+    getFirestore,
+    query,
+    where,
+    doc,
+    getDoc,
+    getDocs,
+    setDoc,
+    collection,
+    onSnapshot
+  } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+  import {
+    getStorage,
+    ref,
+    uploadBytes,
+    getDownloadURL
+  } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-storage.js";
 
-const firebaseConfig = {
-apiKey: "AIzaSyC4Tu-_ZWxpgW3dV9qFVdxf9W_b4szEqYw",
-  authDomain: "jeccanteen-4e566.firebaseapp.com",
-  databaseURL: "https://jeccanteen-4e566-default-rtdb.firebaseio.com",
-  projectId: "jeccanteen-4e566",
-  storageBucket: "jeccanteen-4e566.appspot.com",
-  messagingSenderId: "441614833498",
-  appId: "1:441614833498:web:3ba4a17b1b52cf63b00563",
-  measurementId: "G-QJBRKBVF7N"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyC4Tu-_ZWxpgW3dV9qFVdxf9W_b4szEqYw",
+    authDomain: "jeccanteen-4e566.firebaseapp.com",
+    databaseURL: "https://jeccanteen-4e566-default-rtdb.firebaseio.com",
+    projectId: "jeccanteen-4e566",
+    storageBucket: "jeccanteen-4e566.appspot.com",
+    messagingSenderId: "441614833498",
+    appId: "1:441614833498:web:3ba4a17b1b52cf63b00563",
+    measurementId: "G-QJBRKBVF7N"
+  };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore();
-const storage = getStorage();
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore();
+  const storage = getStorage();
 
 
-const q = query(collection(db, "menu"));
-const unsubscribe = onSnapshot(q, (snapshot) => {
+  const q = query(collection(db, "menu"));
+  const unsubscribe = onSnapshot(q, (snapshot) => {
 
-var table = document.getElementById("myTable");
-var i=0;
+    var table = document.getElementById("myTable");
+    var i = 0;
 
-snapshot.docChanges().forEach((change) => {
-  if (change.type === "added") {
-  var imageurl = "";
+    function FillTboxes(i,name,price) {
+      document.getElementById('NameMod').value = name;
+      document.getElementById('PriceMod').value = price;
+    }
 
-  const image = ref(storage, change.doc.data().photo);
-  getDownloadURL(image).then((url) => {
-    // Insert url into an <img> tag to "download"
-    imageurl = url; 
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === "added") {
+        var imageurl = "";
 
-      var row = table.insertRow(i);
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-      var cell4 = row.insertCell(3);
-      var cell5 = row.insertCell(4);
-      cell1.innerHTML = i+1;
-      cell2.innerHTML = "<img src='"+imageurl+"' style='height:40px;width:40px'>";
-      cell3.innerHTML = change.doc.data().name;
-      cell4.innerHTML = "₹"+change.doc.data().price;
-      cell5.innerHTML = '<button class="w3-round-small w3-btn w3-purple w3-medium" data-toggle="modal" data-target="#exampleModalCenter" onclick="FillTboxes()"><i class="fa fa-pencil-square-o"></i> / <i class="fa fa-trash"></i></button>';
-    i+=1;
+        const image = ref(storage, change.doc.data().photo);
+        getDownloadURL(image).then((url) => {
+            // Insert url into an <img> tag to "download"
+            imageurl = url;
+            var row = table.insertRow(i);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
+            cell1.innerHTML = i + 1;
+            cell2.innerHTML = "<img src='" + imageurl + "' style='height:40px;width:40px'>";
+            cell3.innerHTML = change.doc.data().name;
+            cell4.innerHTML = "₹" + change.doc.data().price;
+            cell5.innerHTML = '<button class="w3-round-small w3-btn w3-purple w3-medium" data-toggle="modal" data-target="#exampleModalCenter"  id="editBtn' + i + '"><i class="fa fa-pencil-square-o"></i> / <i class="fa fa-trash"></i></button>';
 
-  })
-  .catch((error) => { });
+            var editBtnRef = document.getElementById("editBtn" + i); 
+            editBtnRef.addEventListener("click", () => FillTboxes(editBtnRef.id,change.doc.data().name,change.doc.data().price));
+            i += 1;
+          })
+          .catch((error) => {});
 
-  } 
+      }
+    });
   });
-});
-
-
 </script>
