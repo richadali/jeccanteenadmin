bd7df6d057facc4d1c70df79a89024cbc6dd2aa2
JASPREET SINGH <38997592+Jaspreet8843@users.noreply.github.com>
2022-02-23 11:53:16 +0530

    Update home.php
    counters working

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
-
 <?php include 'nav.php';
 ?>
+
 <head>
 </head>
 <!-- Overlay effect when opening sidebar on small screens -->
@@ -18,88 +18,151 @@
   <div class="w3-row-padding w3-margin-bottom ">
     <div class="w3-quarter w3-padding">
       <a href="items.php" style="text-decoration: none">
-      <div class="w3-container w3-red w3-padding-16 w3-hover-shadow">
-        <div class="w3-left"><i class="fa fa-list w3-xxxlarge"></i></div>
-        
-        <div class="w3-right">
-          <h3 id="itemCount"></h3> 
+        <div class="w3-container w3-red w3-padding-16 w3-hover-shadow">
+          <div class="w3-left"><i class="fa fa-list w3-xxxlarge"></i></div>
+
+          <div class="w3-right">
+            <h3 id="itemCount"></h3>
+          </div>
+          <div class="w3-clear"></div>
+          <h4>All Items</h4>
         </div>
-        <div class="w3-clear"></div>
-        <h4>All Items</h4>
-      </div>
     </div></a>
     <div class="w3-quarter w3-padding">
       <a href="additem.php" style="text-decoration: none">
-      <div class="w3-container w3-blue w3-padding-16 w3-hover-shadow">
-        <div class="w3-left"><i class="fa fa-cart-plus w3-xxxlarge"></i></div>
-       
-        <div class="w3-right">
-          <h3>   </h3>
+        <div class="w3-container w3-blue w3-padding-16 w3-hover-shadow">
+          <div class="w3-left"><i class="fa fa-cart-plus w3-xxxlarge"></i></div>
+
+          <div class="w3-right">
+            <h3> </h3>
+          </div>
+          <div class="w3-clear"></div>
+          <h4>Add items</h4>
         </div>
-        <div class="w3-clear"></div>
-        <h4>Add items</h4>
-      </div></a>
+      </a>
     </div>
     <div class="w3-quarter w3-padding">
-    <a href="pendingorders.php" style="text-decoration: none">
-      <div class="w3-container w3-teal w3-padding-16 w3-hover-shadow">
-        
-        <div class="w3-left"><i class="fa fa-clock-o w3-xxxlarge"></i></div>
-        <div class="w3-right">
-          <h3>   </h3>
+      <a href="pendingorders.php" style="text-decoration: none">
+        <div class="w3-container w3-teal w3-padding-16 w3-hover-shadow">
+
+          <div class="w3-left"><i class="fa fa-clock-o w3-xxxlarge"></i></div>
+          <div class="w3-right">
+            <h3 id="pendingCount"> </h3>
+          </div>
+          <div class="w3-clear"></div>
+          <h4>Pending Orders</h4>
         </div>
-        <div class="w3-clear"></div>
-        <h4>Pending Orders</h4>
-      </div></a>
+      </a>
     </div>
     <div class="w3-quarter w3-padding">
       <a href="completedorders.php" style="text-decoration: none">
-      <div class="w3-container w3-orange w3-text-white w3-padding-16 w3-hover-shadow">
-         
-        <div class="w3-left"><i class="fa fa-check-square-o w3-xxxlarge"></i></div>
-        <div class="w3-right">
-          <h3>    </h3>
+        <div class="w3-container w3-orange w3-text-white w3-padding-16 w3-hover-shadow">
+
+          <div class="w3-left"><i class="fa fa-check-square-o w3-xxxlarge"></i></div>
+          <div class="w3-right">
+            <h3 id="completedCount"> </h3>
+          </div>
+          <div class="w3-clear"></div>
+          <h4>Completed Orders</h4>
         </div>
-        <div class="w3-clear"></div>
-        <h4>Completed Orders</h4>
-      </div></a>
+      </a>
     </div>
   </div>
 
 
-<script>
-// Get the Sidebar
-var mySidebar = document.getElementById("mySidebar");
+  <script>
+    // Get the Sidebar
+    var mySidebar = document.getElementById("mySidebar");
 
-// Get the DIV with overlay effect
-var overlayBg = document.getElementById("myOverlay");
+    // Get the DIV with overlay effect
+    var overlayBg = document.getElementById("myOverlay");
 
-// Toggle between showing and hiding the sidebar, and add overlay effect
-function w3_open() {
-  if (mySidebar.style.display === 'block') {
-    mySidebar.style.display = 'none';
-    overlayBg.style.display = "none";
-  } else {
-    mySidebar.style.display = 'block';
-    overlayBg.style.display = "block";
-  }
-}
+    // Toggle between showing and hiding the sidebar, and add overlay effect
+    function w3_open() {
+      if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+        overlayBg.style.display = "none";
+      } else {
+        mySidebar.style.display = 'block';
+        overlayBg.style.display = "block";
+      }
+    }
 
-// Close the sidebar with the close button
-function w3_close() {
-  mySidebar.style.display = "none";
-  overlayBg.style.display = "none";
-}
+    // Close the sidebar with the close button
+    function w3_close() {
+      mySidebar.style.display = "none";
+      overlayBg.style.display = "none";
+    }
+  </script>
 
-const q = query(collection(db, "menu"));
-  const unsubscribe = onSnapshot(q, (snapshot) => {
-    var i = 0;
-    snapshot.docChanges().forEach((change) => {
-      i+=1;
+
+  <script type="module">
+    import {
+      initializeApp
+    } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+    import {
+      getFirestore,
+      query,
+      where,
+      doc,
+      getDoc,
+      getDocs,
+      setDoc,
+      updateDoc,
+      deleteDoc,
+      collection,
+      onSnapshot
+    } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+    import {
+      getStorage,
+      ref,
+      uploadBytes,
+      getDownloadURL
+    } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-storage.js";
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyC4Tu-_ZWxpgW3dV9qFVdxf9W_b4szEqYw",
+      authDomain: "jeccanteen-4e566.firebaseapp.com",
+      databaseURL: "https://jeccanteen-4e566-default-rtdb.firebaseio.com",
+      projectId: "jeccanteen-4e566",
+      storageBucket: "jeccanteen-4e566.appspot.com",
+      messagingSenderId: "441614833498",
+      appId: "1:441614833498:web:3ba4a17b1b52cf63b00563",
+      measurementId: "G-QJBRKBVF7N"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore();
+
+    var q = query(collection(db, "menu"));
+    onSnapshot(q, (snapshot) => {
+      var i = 0;
+      snapshot.docChanges().forEach((change) => {
+        i += 1;
+      });
+      document.getElementById("itemCount").innerHTML = i;
     });
-    document.getElementById("itemCount").innerHTML = i;
-    });
-</script>
 
-</body>
-</html>
+    var q = query(collection(db, "order"), where("status", "==", "Pending"));
+    onSnapshot(q, (snapshot) => {
+      var i = 0;
+      snapshot.docChanges().forEach((change) => {
+        i += 1;
+      });
+      document.getElementById("pendingCount").innerHTML = i;
+    });
+
+    var q = query(collection(db, "order"), where("status", "==", "Completed"));
+    onSnapshot(q, (snapshot) => {
+      var i = 0;
+      snapshot.docChanges().forEach((change) => {
+        i += 1;
+      });
+      document.getElementById("completedCount").innerHTML = i;
+    });
+  </script>
+
+
+  </body>
+
+  </html>
